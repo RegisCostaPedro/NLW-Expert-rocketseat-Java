83d398181a733f3857ceced137dec9c4402cc25e
regis <regiscostapedro0705@gmail.com>
2024-02-08 17:36:05 -0300

    Feat: Creation of DTOs

---
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/dto/VerifyHasCeriticationDTO.java
@@ -0,0 +1,14 @@
+package com.rocketseat.certification_nlw.modules.students.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VerifyHasCeriticationDTO {
+    private String email;
+    private String technology;
+    
+}
