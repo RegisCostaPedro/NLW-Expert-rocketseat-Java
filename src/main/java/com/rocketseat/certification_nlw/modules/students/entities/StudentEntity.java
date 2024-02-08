f73979323b44868bab4f88b495002e42e184cdb8
regis <regiscostapedro0705@gmail.com>
2024-02-08 17:51:32 -0300

    Perf: adding annotations using Lombok, creating relationships in database tables using JPA

--- src/main/java/com/rocketseat/certification_nlw/modules/students/entities/StudentEntity.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/entities/StudentEntity.java
@@ -1,6 +1,15 @@
 package com.rocketseat.certification_nlw.modules.students.entities;
+import java.time.LocalDateTime;
 import java.util.*;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +17,18 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "students")
 public class StudentEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    
+    @Column(unique = true, nullable = false)
     private String email;
-    private List<CertificationStudentEntity> certificationStudentEntity;
+
+    @OneToMany(mappedBy = "studentEntity")
+     private List<CertificationStudentEntity> certificationStudentEntity;
+
+     @CreationTimestamp
+    private LocalDateTime createdAt;
 }
